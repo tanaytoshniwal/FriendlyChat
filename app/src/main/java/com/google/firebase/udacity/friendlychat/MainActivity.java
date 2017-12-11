c9c632869a938d7058c40de09b89708df7b564e2
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-12-11 15:19:11 +0530

    on click event

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -116,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
                 // TODO: Send messages on click
                 FriendlyMessage friendlyMessage = new FriendlyMessage(mMessageEditText.getText().toString(), mUsername, null);
                 mMessagesDatabaseReference.push().setValue(friendlyMessage);
-                // Clear input box
+                // This is to Clear input box
                 mMessageEditText.setText("");
             }
         });
